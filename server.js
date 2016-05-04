3423d1c7ffb0c27343fbcc17e4b1a4a3e5cee922
Eli Yazdi <eli6093@gmail.com>
2016-05-04 09:32:32 -0400

    more

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ var bot = new TelegramBot(token, {polling:true});
 
 bot.on('new_chat_participant', function(msg){
   var chatId = msg.chat.id;
+  var photo = "M1aF0.png";
+  bot.sendPhoto(chatId, photo, {caption: 'Hello and welcome to the WWDC Scholars Telegram group! Here you can discuss pretty much anything with us, so feel free to start chatting!'});
+});
 
+// Matches /echo [whatever]
+bot.onText("/welcome", function (msg, match) {
+  var chatId = msg.chat.id;
   var photo = "M1aF0.png";
   bot.sendPhoto(chatId, photo, {caption: 'Hello and welcome to the WWDC Scholars Telegram group! Here you can discuss pretty much anything with us, so feel free to start chatting!'});
 });
